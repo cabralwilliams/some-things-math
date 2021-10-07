0d1f8c468f4c91cc772ccd8f64270d81dea2926a
Cabral Williams <cabral.williams@gmail.com>
2021-10-07 07:51:01 -0500

    created linear systems practice

---
+++ assets/js/linearControl1.js
@@ -0,0 +1,284 @@
+
+var selectorDiv = document.querySelector("#selectorDiv");
+var quizDiv = document.querySelector("#quizDiv");
+
+//Administrative Coding
+var questionCount = 0;
+var correctCount = 0;
+var totalTime = 0;
+var last10Linear = JSON.parse(localStorage.getItem("last10Linear")) || [];
+var thisQuiz = [];
+var nextObject, qOption, question, stResponse, corAnswers, qNumber, storedObject;
+var timeController;
+clearInterval(timeController);
+
+var saveLast10 = function() {
+    localStorage.setItem("last10Linear",JSON.stringify(last10Linear));
+};
+
+var addClassToElement = function(element,classToAdd) {
+    element.classList.add(classToAdd);
+};
+
+var createNextObject = function() {
+    var outputDiv = document.createElement("div");
+};
+
+function generateStatusDisplay() {
+    var overallDiv = document.createElement("div");
+    addClassToElement(overallDiv,"row-to-flex");
+    addClassToElement(overallDiv,"justify-between");
+    var questionDiv = document.createElement("div");
+    questionDiv.id = "questionNumber";
+    var timerDiv = document.createElement("div");
+    var timeSpan1 = document.createElement("span");
+    timeSpan1.textContent = "Ellapsed Time: ";
+    var timeSpan2 = document.createElement("span");
+    timeSpan2.id = "timeEllapsed";
+    questionDiv.textContent = `Question ${questionCount} of 10`;
+    timeSpan2.textContent = formatTimeToSecond(totalTime);
+    timerDiv.append(timeSpan1,timeSpan2);
+    selectorDiv.innerHTML = "";
+    overallDiv.append(questionDiv,timerDiv);
+    selectorDiv.appendChild(overallDiv);
+}
+
+function updateTime() {
+    totalTime += 1000;
+    document.querySelector("#timeEllapsed").textContent = formatTimeToSecond(totalTime);
+}
+
+var generateSelectorElement = function() {
+    var selectorForm = document.createElement("form");
+    addClassToElement(selectorForm,"lonely");
+
+    //Submit button
+    var buttonEl = document.createElement("button");
+    buttonEl.type = "submit";
+    buttonEl.className = "my-basic-button";
+    buttonEl.textContent = "Start";
+    selectorForm.append(buttonEl);
+    selectorDiv.innerHTML = "";
+    selectorDiv.append(selectorForm);
+};
+
+var generatePreviousResults = function() {
+    quizDiv.innerHTML = "";
+    var outputDiv = document.createElement("div");
+    addClassToElement(outputDiv,"row-to-flex-2");
+    //addClassToElement(outputDiv,"justify-around");
+    var displayButton = document.createElement("button");
+    addClassToElement(displayButton,"my-basic-button");
+    var clearButton = document.createElement("button");
+    addClassToElement(clearButton,"my-basic-button");
+    displayButton.id = "displayData";
+    clearButton.id = "clearData";
+    displayButton.textContent = "Show Previous Results";
+    clearButton.textContent = "Clear Previous Results";
+    var displayDiv = document.createElement("div");
+    var clearDiv = document.createElement("div");
+    displayDiv.appendChild(displayButton);
+    clearDiv.appendChild(clearButton);
+    outputDiv.append(displayDiv,clearDiv);
+    quizDiv.appendChild(outputDiv);
+};
+
+var generateNextObject = function() {
+    nextObject = new AllLinearClasses().ob;
+    questionCount++;
+    var questionDisplayDiv = document.createElement("div");
+    addClassToElement(questionDisplayDiv,"column-to-flex");
+    addClassToElement(questionDisplayDiv,"width250");
+    addClassToElement(questionDisplayDiv,"align-items-center");
+    var questionHolder = document.createElement("div");
+    addClassToElement(questionHolder,"column-to-flex");
+    addClassToElement(questionHolder,"align-items-center");
+    questionHolder.innerHTML = `<ul><li>${nextObject.questionArray[0]}</li><li>${nextObject.questionArray[1]}</li></ul><p>${nextObject.questionArray[2]}</p>`;
+    var answerForm = document.createElement("form");
+    addClassToElement(answerForm,"column-to-flex");
+    var textInput = document.createElement("input");
+    textInput.type = "text";
+    textInput.id = "studentAnswer";
+    textInput.name = "studentAnswer";
+    var buttonElement = document.createElement("button");
+    addClassToElement(buttonElement,"my-basic-button");
+    buttonElement.type = "submit";
+    buttonElement.textContent = "Submit";
+    answerForm.append(textInput,buttonElement);
+    questionDisplayDiv.append(questionHolder,answerForm);
+    quizDiv.innerHTML = "";
+    quizDiv.appendChild(questionDisplayDiv);
+};
+
+var setUpQuiz = function(event) {
+    event.preventDefault();
+    thisQuiz = [];
+    generateNextObject();
+    generateStatusDisplay();
+    storedObject = {};
+    console.log(qNumber);
+    console.log(questionCount);
+    timeController = setInterval(updateTime,1000);
+};
+
+var createResultOb = function(question,response,answers,wasCorrect) {
+    var output = {};
+    output.question = question;
+    output.response = response;
+    output.correctAnswers = answers.join(", ");
+    output.wasCorrect = wasCorrect;
+    return output;
+};
+
+var resultRow = function(questionNumber,question,studentResponse,correctAnswers,wasCorrect) {
+    var rowEl = document.createElement("tr");
+    var td1 = document.createElement("td");
+    addClassToElement(td1,"border-Bottom-Right-1");
+    addClassToElement(td1,"tPadding-4");
+    td1.textContent = questionNumber;
+    var td2 = document.createElement("td");
+    addClassToElement(td2,"border-Bottom-Right-1");
+    addClassToElement(td2,"tPadding-4");
+    td2.innerHTML = question;
+    var td3 = document.createElement("td");
+    addClassToElement(td3,"border-Bottom-Right-1");
+    addClassToElement(td3,"tPadding-4");
+    td3.textContent = studentResponse;
+    var td4 = document.createElement("td");
+    addClassToElement(td4,"border-Bottom-Right-1");
+    addClassToElement(td4,"tPadding-4");
+    td4.innerHTML = correctAnswers;
+    var td5 = document.createElement("td");
+    addClassToElement(td5,"border-Bottom-1");
+    addClassToElement(td5,"tPadding-4");
+    td5.textContent = wasCorrect;
+    rowEl.append(td1,td2,td3,td4,td5);
+    return rowEl;
+};
+
+var checkSubmittedAnswer = function(event) {
+    event.preventDefault();
+    stResponse = document.querySelector("#studentAnswer").value.trim();
+    corAnswers = nextObject.questionArray[3];
+    console.log(corAnswers);
+    console.log(stResponse);
+    var answerFound = false;
+    for(var i = 0; i < corAnswers.length; i++) {
+        if(convertStandard(stripSpaces(stResponse)) === convertStandard(stripSpaces(String(corAnswers[i])))) {
+            answerFound = true;
+            break;
+        }
+    }
+    var resultOb = createResultOb(nextObject.questionArray[2],stResponse,nextObject.questionArray[3],answerFound);
+    thisQuiz.push(resultOb);
+    if(questionCount === 10) {
+        clearInterval(timeController);
+        storedObject.resultArray = thisQuiz;
+        storedObject.ellapsedTime = totalTime;
+        storedObject.quizDate = moment().format("MM/D/YYYY");
+        var newLast10 = [storedObject];
+        if(last10Linear.length === 10) {
+            for(i = 0; i < 9; i++) {
+                newLast10.push(last10Linear[i]);
+            }
+        } else {
+            for(i = 0; i < last10Linear.length; i++) {
+                newLast10.push(last10Linear[i]);
+            }
+        }
+        last10Linear = newLast10;
+        saveLast10();
+        var displayedResults = tabulateResults(storedObject);
+        quizDiv.innerHTML = "";
+        generateSelectorElement();
+        quizDiv.appendChild(displayedResults);
+    } else {
+        generateNextObject();
+        generateStatusDisplay();
+    }
+};
+
+
+
+function tabulateResults(resultObject) {
+    var tableHeaders = document.createElement("tr");
+    var th1 = document.createElement("th");
+    addClassToElement(th1,"border-Bottom-Right-1");
+    addClassToElement(th1,"tPadding-4");
+    th1.textContent = "Question #";
+    var th2 = document.createElement("th");
+    addClassToElement(th2,"border-Bottom-Right-1");
+    addClassToElement(th2,"tPadding-4");
+    th2.textContent = "Question";
+    var th3 = document.createElement("th");
+    addClassToElement(th3,"border-Bottom-Right-1");
+    addClassToElement(th3,"tPadding-4");
+    th3.textContent = "Your Response";
+    var th4 = document.createElement("th");
+    addClassToElement(th4,"border-Bottom-Right-1");
+    addClassToElement(th4,"tPadding-4");
+    th4.textContent = "Correct Answer(s)";
+    var th5 = document.createElement("th");
+    addClassToElement(th5,"border-Bottom-1");
+    addClassToElement(th5,"tPadding-4");
+    th5.textContent = "Correct?";
+    tableHeaders.append(th1,th2,th3,th4,th5);
+    var dateDiv = document.createElement("div");
+    dateDiv.textContent = "Date: " + resultObject.quizDate;
+    var topicDiv = document.createElement("div");
+    topicDiv.textContent = "Topic: Linear Systems";
+    var timeDiv = document.createElement("div");
+    timeDiv.textContent = "Ellapsed Time: " + formatTimeToSecond(resultObject.ellapsedTime);
+    var performanceDiv = document.createElement("div");
+    var totalCorrect = 0;
+    var tableEl = document.createElement("table");
+    addClassToElement(tableEl,"borderColor1All");
+    tableEl.appendChild(tableHeaders);
+    for(var i = 0; i < resultObject.resultArray.length; i++) {
+        var nextRow = resultRow(i+1,resultObject.resultArray[i].question,resultObject.resultArray[i].response,resultObject.resultArray[i].correctAnswers,resultObject.resultArray[i].wasCorrect);
+        if(resultObject.resultArray[i].wasCorrect) {
+            totalCorrect++;
+        }
+        tableEl.appendChild(nextRow);
+    }
+    performanceDiv.textContent = "Performance: " + totalCorrect + "/10";
+    var tbodyEl = document.createElement("tbody");
+    tbodyEl.appendChild(tableEl);
+    var outputDiv = document.createElement("div");
+    addClassToElement(outputDiv,"column-to-flex");
+    outputDiv.append(dateDiv,topicDiv,timeDiv,performanceDiv,tbodyEl);
+    return outputDiv;
+}
+
+function clearAllData(event) {
+    event.preventDefault();
+    localStorage.removeItem("last10Linear");
+    last10Linear = [];
+    quizDiv.innerHTML = "";
+}
+
+function showPriorResults(event) {
+    event.preventDefault();
+    quizDiv.innerHTML = "";
+    for(var i = 0; i < last10Linear.length; i++) {
+        var nextResult = tabulateResults(last10Linear[i]);
+        quizDiv.appendChild(nextResult);
+    }
+}
+
+
+generateSelectorElement();
+generatePreviousResults();
+
+
+//setUpQuiz();
+
+selectorDiv.addEventListener("submit", setUpQuiz);
+quizDiv.addEventListener("submit", checkSubmittedAnswer);
+quizDiv.addEventListener("click", function(event) {
+    if(event.target.id === "clearData") {
+        clearAllData(event);
+    } else if(event.target.id === "displayData") {
+        showPriorResults(event);
+    }
+});
---
+++ assets/js/linearObjects.js
@@ -0,0 +1,241 @@
+//Strips whitespace from input answers
+var stripSpaces = function(inputString = "some string") {
+    var output = inputString.replaceAll(" ", "");
+    return output;
+};
+
+//Formats numbers into time formats
+function formatTimeToSecond(inputTime = 1000) {
+    if(inputTime === 0) {
+        return "0";
+    }
+    var remain = inputTime;
+    var output = "";
+    var hour = 3600000;
+    var tenM = 600000;
+    var m = 60000;
+    var tenS = 10000;
+    var s = 1000;
+    var hourAdded = false;
+    if(remain >= hour) {
+        output += Math.floor(remain/hour) + ":";
+        remain -= Math.floor(remain/hour)*hour;
+        hourAdded = true;
+    }
+    var tenMAdded = false;
+    if(remain >= tenM || hourAdded) {
+        output += Math.floor(remain/tenM);
+        remain -= Math.floor(remain/tenM)*tenM;
+        tenMAdded = true;
+    }
+    var mAdded = false;
+    if(remain >= m || tenMAdded) {
+        output += Math.floor(remain/m) + ":";
+        remain -= Math.floor(remain/m)*m;
+        mAdded = true;
+    }
+    var tenSAdded = false;
+    if(remain >= tenS || mAdded) {
+        output += Math.floor(remain/tenS);
+        remain -= Math.floor(remain/tenS)*tenS;
+        tenSAdded = true;
+    }
+    if(remain >= s || tenSAdded) {
+        output += Math.floor(remain/s);
+        remain -= Math.floor(remain/s)*s;
+    }
+    return output;
+}
+
+function reselectIfZero(positiveLimit = 10) {
+    var posLimit = positiveLimit <= 0 ? -(positiveLimit - 1) : positiveLimit + 1;
+    var output = -posLimit + Math.floor(Math.random()*(2*posLimit + 1));
+    while(output === 0) {
+        output = -posLimit + Math.floor(Math.random()*(2*posLimit + 1));
+    }
+    return output;
+}
+
+function convertStandard(quadString) {
+    var output = quadString.replace("<sup>", "^");
+    output = output.replace("</sup>", "");
+    output = output.replaceAll("<i>", "");
+    output = output.replaceAll("</i>", "");
+    return output;
+}
+
+function formatStandardLinear(coA,coB,coC) {
+    var output = "";
+    if(typeof coA === "number") {
+        if(coA === -1) {
+            output += "-";
+        } else if(coA !== 1) {
+            output += coA;
+        }
+    } else if(typeof coA === "string") {
+        output += coA;
+    }
+    output += "<i>x</i>";
+    if(typeof coB === "number") {
+        if(coB < 0) {
+            output += " - ";
+            if(coB !== -1) {
+                output += -coB;
+            }
+        } else if(coB > 0) {
+            output += " + ";
+            if(coB !== 1) {
+                output += coB;
+            }
+        }
+    } else if(typeof coB === "string") {
+        output += " + " + coB;
+    }
+    output += "<i>y</i> = " + coC;
+    return output;
+}
+
+class ParallelLines1 {
+    constructor() {
+        this.coA1 = reselectIfZero(12);
+        this.coB1 = reselectIfZero(12);
+        this.solX = reselectIfZero(3);
+        this.solY = reselectIfZero(3);
+        this.coC1 = this.coA1*this.solX + this.coB1*this.solY;
+        this.multiple = reselectIfZero(4);
+        while(this.multiple === 1) {
+            this.multiple = reselectIfZero(4);
+        }
+        this.coA2 = this.coA1*this.multiple;
+        this.coB2 = this.coB1*this.multiple;
+        this.coC2 = this.coA2*this.solX + this.coB2*this.solY + reselectIfZero(6);
+        this.questionArray = this.createQuestion();
+    }
+
+    createQuestion() {
+        var coefficientArray = [this.coA1,this.coB1,this.coA2,this.coB2];
+        var changeInt = Math.floor(Math.random()*4);
+        var answer = coefficientArray[changeInt];
+        var newCoefficients = [];
+        for(var i = 0; i < 4; i++) {
+            if(i !== changeInt) {
+                newCoefficients.push(coefficientArray[i]);
+            } else {
+                newCoefficients.push("K");
+            }
+        }
+        var line1 = formatStandardLinear(newCoefficients[0],newCoefficients[1],this.coC1);
+        var line2 = formatStandardLinear(newCoefficients[2],newCoefficients[3],this.coC2);
+        var question = Math.random() < 0.5 ? "If the two lines above are parallel, what is the value of K?" : "If the system above has no solution, what is the value of K?";
+        var output = [];
+        if(Math.random() < 0.5) {
+            output.push(line1);
+            output.push(line2);
+        } else {
+            output.push(line2);
+            output.push(line1);
+        }
+        output.push(question);
+        output.push([answer]);
+        return output;
+    }
+}
+
+class EquivalentLines1 {
+    constructor() {
+        this.coA1 = reselectIfZero(12);
+        this.coB1 = reselectIfZero(12);
+        this.solX = reselectIfZero(3);
+        this.solY = reselectIfZero(3);
+        this.coC1 = this.coA1*this.solX + this.coB1*this.solY;
+        this.multiple = reselectIfZero(4);
+        while(this.multiple === 1) {
+            this.multiple = reselectIfZero(4);
+        }
+        this.coA2 = this.coA1*this.multiple;
+        this.coB2 = this.coB1*this.multiple;
+        this.coC2 = this.coA2*this.solX + this.coB2*this.solY;
+        this.questionArray = this.createQuestion();
+    }
+
+    createQuestion() {
+        var coefficientArray = [this.coA1,this.coB1,this.coC1,this.coA2,this.coB2,this.coC2];
+        var changeInt = Math.floor(Math.random()*6);
+        var answer = coefficientArray[changeInt];
+        var newCoefficients = [];
+        for(var i = 0; i < 6; i++) {
+            if(i !== changeInt) {
+                newCoefficients.push(coefficientArray[i]);
+            } else {
+                newCoefficients.push("K");
+            }
+        }
+        var line1 = formatStandardLinear(newCoefficients[0],newCoefficients[1],newCoefficients[2]);
+        var line2 = formatStandardLinear(newCoefficients[3],newCoefficients[4],newCoefficients[5]);
+        var question = Math.random() < 0.5 ? "If the two lines above have more than one point in common, what is the value of K?" : "If the system above has an infinite number of solutions, what is the value of K?";
+        var output = [];
+        if(Math.random() < 0.5) {
+            output.push(line1);
+            output.push(line2);
+        } else {
+            output.push(line2);
+            output.push(line1);
+        }
+        output.push(question);
+        output.push([answer]);
+        return output;
+    }
+}
+
+class IntersectingLines1 {
+    constructor() {
+        this.coA1 = reselectIfZero(12);
+        this.coB1 = reselectIfZero(12);
+        this.coA2 = reselectIfZero(12);
+        this.coB2 = reselectIfZero(12);
+        while(this.coA1*this.coB2 === this.coA2*this.coB1) {
+            this.coA2 = reselectIfZero(12);
+            this.coB2 = reselectIfZero(12);
+        }
+        this.solX = reselectIfZero(9);
+        this.solY = reselectIfZero(9);
+        this.coC1 = this.coA1*this.solX + this.coB1*this.solY;
+        this.coC2 = this.coA2*this.solX + this.coB2*this.solY;
+        this.questionArray = this.createQuestion();
+    }
+
+    createQuestion() {
+        var line1 = formatStandardLinear(this.coA1,this.coB1,this.coC1);
+        var line2 = formatStandardLinear(this.coA2,this.coB2,this.coC2);
+        var answer1 = `(${this.solX}, ${this.solY})`;
+        var answer2 = `${this.solX}, ${this.solY}`;
+        var question = "What is the solution (<i>x</i>, <i>y</i>) to the system above?";
+        return [line1,line2,question,[answer1,answer2]];
+    }
+}
+
+class AllLinearClasses {
+    constructor(selectorInt = Math.floor(Math.random()*3)) {
+        this.selectorInt = selectorInt;
+        this.ob = this.createObject();
+    }
+
+    createObject() {
+        var ob;
+        switch(this.selectorInt) {
+            case 0:
+                ob = new ParallelLines1();
+                break;
+            case 1:
+                ob = new EquivalentLines1();
+                break;
+            case 2:
+                ob = new IntersectingLines1();
+                break;
+            default:
+                ob = new ParallelLines1();
+                break;
+        }
+        return ob;
+    }
+}
